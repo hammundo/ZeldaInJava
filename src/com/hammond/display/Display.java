088a3d55217405f703d0926251cb22bde17b233f
Josh <6570488+hammundo@users.noreply.github.com>
2019-02-01 10:00:18 +0000

    Add files via upload

---
+++ src/com/hammond/display/Display.java
@@ -0,0 +1,50 @@
+package com.hammond.display;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Display {
+
+    private JFrame frame;
+    private Canvas canvas;
+
+    private String title;
+    private int width, height;
+
+    public Display(String title, int width, int height) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+
+        createDisplay();
+    }
+
+    private void createDisplay(){
+        //Set up the frame to contains the game canvas
+        frame = new JFrame(title);
+        frame.setSize(width, height);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        //Create the canvas and specify pref, max and min values to be the same to ensure the canvas cannot be resized.
+        canvas = new Canvas();
+        canvas.setPreferredSize(new Dimension(width, height));
+        canvas.setMaximumSize(new Dimension(width, height));
+        canvas.setMinimumSize(new Dimension(width, height));
+        canvas.setFocusable(false);
+
+        frame.add(canvas);
+        frame.pack();
+    }
+
+    public Canvas getCanvas() {
+        return canvas;
+    }
+
+    public JFrame getFrame() {
+        return frame;
+    }
+
+}
