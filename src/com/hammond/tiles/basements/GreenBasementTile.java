a98475dcd9b67a3d33b0243e5058d1cd197c2463
Josh <6570488+hammundo@users.noreply.github.com>
2019-02-01 10:09:26 +0000

    Add files via upload

---
+++ src/com/hammond/tiles/basements/GreenBasementTile.java
@@ -0,0 +1,11 @@
+package com.hammond.tiles.basements;
+
+import com.hammond.graphics.Assets;
+import com.hammond.tiles.Tile;
+
+public class GreenBasementTile extends Tile {
+
+    public GreenBasementTile(int id) {
+        super(Assets.green_basement, id);
+    }
+}
---
+++ src/com/hammond/tiles/basements/WhiteBasementTile.java
@@ -0,0 +1,11 @@
+package com.hammond.tiles.basements;
+
+import com.hammond.graphics.Assets;
+import com.hammond.tiles.Tile;
+
+public class WhiteBasementTile extends Tile {
+
+    public WhiteBasementTile(int id) {
+        super(Assets.white_basement, id);
+    }
+}
